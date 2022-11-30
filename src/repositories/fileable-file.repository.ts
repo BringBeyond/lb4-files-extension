18ca405d87d4c798bdf02432aaba015083b9ea25
Max Kirschning <max.kirschning@gmail.com>
2022-11-30 13:46:20 +0100

    Fix Schema

--- src/repositories/fileable-file.repository.ts
+++ src/repositories/fileable-file.repository.ts
@@ -1,11 +1,10 @@
+import {AuthUser, DefaultUserModifyCrudRepository} from '@BringBeyond/lb4-base-extension';
 import {Getter, inject} from '@loopback/core';
 import {BelongsToAccessor, repository} from '@loopback/repository';
 import {AuthenticationBindings} from 'loopback4-authentication';
 import {FileRepository} from '.';
 import {FilesdbDataSource} from '../datasources';
 import {File, FileableFile, FileableFileRelations} from '../models';
-import {AuthUser} from '@BringBeyond/lb4-base-extension';
-import {DefaultUserModifyCrudRepository} from '@BringBeyond/lb4-base-extension';
 
 export class FileableFileRepository extends DefaultUserModifyCrudRepository<
   FileableFile,
@@ -20,10 +19,10 @@ export class FileableFileRepository extends DefaultUserModifyCrudRepository<
     fileRepositoryGetter: Getter<FileRepository>,
     @inject.getter(AuthenticationBindings.CURRENT_USER)
     protected readonly getCurrentUser: Getter<AuthUser | undefined>,
-    // @repository.getter('FileRepository') protected fileRepositoryGetter: Getter<FileRepository>,
-  ) {
+  ) // @repository.getter('FileRepository') protected fileRepositoryGetter: Getter<FileRepository>,
+  {
     super(FileableFile, dataSource, getCurrentUser);
-    this.file = this.createBelongsToAccessorFor('file_id', fileRepositoryGetter);
+    this.file = this.createBelongsToAccessorFor('file', fileRepositoryGetter);
     this.registerInclusionResolver('file', this.file.inclusionResolver);
   }
 }
